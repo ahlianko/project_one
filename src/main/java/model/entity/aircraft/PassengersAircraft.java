597cc046932dd429b973239e4084c635e3106aac
Andrey Glyanko <glyanko.andrey@gmail.com11>
2017-12-12 16:35:27 +0200

    delete

--- src/main/java/model/entity/aircraft/PassengersAircraft.java
+++
@@ -1,7 +0,0 @@
-package model.entity.aircraft;
-
-public class PassengersAircraft extends Aircraft {
-    public PassengersAircraft(String name, int passengers, int cargo, int flyRange, int fuelConsumption) {
-        super(name, passengers, cargo, flyRange, fuelConsumption);
-    }
-}
